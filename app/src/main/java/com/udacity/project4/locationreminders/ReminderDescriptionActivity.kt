86404a8f0ae7025a1eeca93e47e0c511de329ef3
Temilola <temz4rill@gmail.com>
2021-09-04 13:56:12 -0400

    Completed notification and deep linking to details screen

--- app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -26,12 +26,15 @@ class ReminderDescriptionActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityReminderDescriptionBinding
+    private lateinit var reminderDataItem: ReminderDataItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_reminder_description
         )
-//        TODO: Add the implementation of the reminder details
+        reminderDataItem = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem
+        binding.reminderDataItem = reminderDataItem
     }
 }
